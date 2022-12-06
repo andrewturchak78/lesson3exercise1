1a950357b06f8faa293f4853a2aea193d7ade0b6
Andrei <andrewturchak78@mail.ru>
2022-12-06 21:51:57 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 500;
+        int rublesPerMiles = 20;
+        int miles = ticket / rublesPerMiles;
+        System.out.println (miles);
+    }
+}
